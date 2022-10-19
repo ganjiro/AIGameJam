b5f9f5d399088a5b7268109cac127d7f03a85140
ganjiro <52581772+ganjiro@users.noreply.github.com>
2022-10-20 01:17:43 +0200

    Torch and Map Matrix Implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,8 +8,10 @@ public class PlayerController : MonoBehaviour
     public UnityEvent moveEnemies;
     public float moveSpeed = 5f;
     public Transform movePoint;
+    public Transform flashLight;
     public LayerMask cantMove;
     public EnemyManager _enemyManager;
+    public int flashlightState = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -47,5 +49,121 @@ public class PlayerController : MonoBehaviour
                 StartCoroutine(_enemyManager.moveAgents());
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            switch (flashlightState)
+            {
+                case (0):
+                    flashlightState = 1;
+                    flashLight.localPosition = new Vector3(1.2f, -1.2f, 0f);
+                    flashLight.rotation = Quaternion.Euler(0, 0, -45f);
+                    break;
+
+                case (1):
+                    flashlightState = 2;
+                    flashLight.localPosition = new Vector3(0f, -1.5f, 0f);
+                    flashLight.rotation = Quaternion.Euler(0, 0, 90f);
+                    break;
+                case (2):
+                    flashlightState = 3;
+                    flashLight.localPosition = new Vector3(-1.2f, -1.2f, 0f);
+                    flashLight.rotation = Quaternion.Euler(0, 0, 45);
+                    break;
+                case (3):
+                    flashlightState = 4;
+                    flashLight.localPosition = new Vector3(-1.5f, 0f, 0f);
+                    flashLight.rotation = Quaternion.Euler(0, 0, 0);
+                    break;
+                case (4):
+                    flashlightState = 5;
+                    flashLight.localPosition = new Vector3(-1.2f, 1.2f, 0f);
+                    flashLight.rotation = Quaternion.Euler(0, 0, -45f);
+                    break;
+                case (5):
+                    flashlightState = 6;
+                    flashLight.localPosition = new Vector3(0f, 1.5f, 0f);
+                    flashLight.rotation = Quaternion.Euler(0, 0, 90f);
+                    break;
+                case (6):
+                    flashlightState = 7;
+                    flashLight.localPosition = new Vector3(1.2f, 1.2f, 0f);
+                    flashLight.rotation = Quaternion.Euler(0, 0, 45f);
+                    break;
+                case (7):
+                    flashlightState = 0;
+                    flashLight.localPosition = new Vector3(1.5f, 0f, 0f);
+                    flashLight.rotation = Quaternion.Euler(0, 0, 0f);
+                    break;
+                default:
+                    break;
+            }
+        }
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+
+                switch (flashlightState)
+                {
+                    case (0):
+                        flashlightState = 7;
+                        flashLight.localPosition = new Vector3(1.2f, 1.2f, 0f);
+                        flashLight.rotation = Quaternion.Euler(0, 0, 45f);
+                        break;
+                                              
+
+                    case (1):
+                        flashlightState = 0;
+                        flashLight.localPosition = new Vector3(1.5f, 0f, 0f);
+                        flashLight.rotation = Quaternion.Euler(0, 0, 0f);
+                        break;
+
+                        
+                    case (2):
+                        flashlightState = 1;
+                        flashLight.localPosition = new Vector3(1.2f, -1.2f, 0f);
+                        flashLight.rotation = Quaternion.Euler(0, 0, -45f);
+                        break;
+
+                        
+                    case (3):
+                        flashlightState = 2;
+                        flashLight.localPosition = new Vector3(0f, -1.5f, 0f);
+                        flashLight.rotation = Quaternion.Euler(0, 0, 90f);
+                        break;
+                        
+                    case (4):
+                        flashlightState = 3;
+                        flashLight.localPosition = new Vector3(-1.2f, -1.2f, 0f);
+                        flashLight.rotation = Quaternion.Euler(0, 0, 45);
+                        break;
+                        
+                    case (5):
+                        flashlightState = 4;
+                        flashLight.localPosition = new Vector3(-1.5f, 0f, 0f);
+                        flashLight.rotation = Quaternion.Euler(0, 0, 0);
+                        break;
+                        
+                    case (6):
+                        flashlightState = 5;
+                        flashLight.localPosition = new Vector3(-1.2f, 1.2f, 0f);
+                        flashLight.rotation = Quaternion.Euler(0, 0, -45f);
+                        break;                       
+                    case (7):
+                        flashlightState = 6;
+                        flashLight.localPosition = new Vector3(0f, 1.5f, 0f);
+                        flashLight.rotation = Quaternion.Euler(0, 0, 90f);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
     }
+
+    
+
+    
+
+
 }
--- Assets/Scripts/Regenerate.cs
+++ Assets/Scripts/Regenerate.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using System.Linq;
 
 public class Regenerate : MonoBehaviour
 {
@@ -15,7 +16,6 @@ public class Regenerate : MonoBehaviour
     public GameObject enemyPrefab;
     public GameObject playerPrefab;
     public LayerMask cantMove;
-
 
     // Start is called before the first frame update
     void Start()
@@ -82,8 +82,12 @@ public class Regenerate : MonoBehaviour
             goal.transform.position = new Vector3(xP, yP, 0f);
 
         }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            getFullStateMatrix();
+        }
 
-    }
+     }
 
     private void spawnEnemy()
     {
@@ -98,5 +102,121 @@ public class Regenerate : MonoBehaviour
 
         GameObject instantiatedObject = Instantiate(enemyPrefab, new Vector3(x, y, 0f), Quaternion.identity);
         instantiatedObject.transform.SetParent(agents.transform);
+    }
+
+
+    public int[,] getFullStateMatrix()
+    {
+        int[,] stateMatrix = new int[10, 10];
+
+        foreach (Transform child in obstacles.transform)
+        {
+            stateMatrix[(int)(child.position.x + 4.5), Mathf.Abs((int)(child.position.y - 4.5))] = 1;
+        }
+
+        foreach (Transform child in agents.transform)
+        {
+            stateMatrix[(int)(child.position.x + 4.5), Mathf.Abs((int)(child.position.y - 4.5))] = 2;
+        }
+
+
+        stateMatrix[(int)(goal.transform.position.x + 4.5), Mathf.Abs((int)(goal.transform.position.y - 4.5))] = 3;
+        stateMatrix[(int)(player.transform.position.x + 4.5), Mathf.Abs((int)(player.transform.position.y - 4.5))] = 4;
+
+        int flashState = player.GetComponent<PlayerController>().flashlightState;
+
+        switch (flashState)
+        {
+            case (0):
+                try
+                {
+                    stateMatrix[(int)(player.transform.position.x + 4.5), Mathf.Abs((int)(player.transform.position.y - 4.5)) + 1] = 5;
+                    stateMatrix[(int)(player.transform.position.x + 4.5), Mathf.Abs((int)(player.transform.position.y - 4.5)) + 2] = 5;
+                }
+                catch
+                { }
+
+                break;
+
+            case (1):
+                try
+                {
+                    stateMatrix[(int)(player.transform.position.x + 4.5) + 1, Mathf.Abs((int)(player.transform.position.y - 4.5)) - 1] = 5;
+                    stateMatrix[(int)(player.transform.position.x + 4.5) + 2, Mathf.Abs((int)(player.transform.position.y - 4.5)) - 2] = 5;
+                }
+                catch 
+                { }
+                break;
+            case (2):
+                try
+                {
+                    stateMatrix[(int)(player.transform.position.x + 4.5), Mathf.Abs((int)(player.transform.position.y - 4.5)) - 1] = 5;
+                    stateMatrix[(int)(player.transform.position.x + 4.5), Mathf.Abs((int)(player.transform.position.y - 4.5)) - 2] = 5;
+                }
+                catch 
+                { }
+                break;
+            case (3):
+                try
+                {
+                    stateMatrix[(int)(player.transform.position.x + 4.5) - 1, Mathf.Abs((int)(player.transform.position.y - 4.5)) - 1] = 5;
+                    stateMatrix[(int)(player.transform.position.x + 4.5) - 2, Mathf.Abs((int)(player.transform.position.y - 4.5)) - 2] = 5;
+                }
+                catch
+                { }
+                break;
+            case (4):
+                try
+                {
+                    stateMatrix[(int)(player.transform.position.x + 4.5) - 1, Mathf.Abs((int)(player.transform.position.y - 4.5))] = 5;
+                    stateMatrix[(int)(player.transform.position.x + 4.5) - 2, Mathf.Abs((int)(player.transform.position.y - 4.5))] = 5;
+                }
+                catch 
+                { }
+                break;
+            case (5):
+                try
+                {
+                    stateMatrix[(int)(player.transform.position.x + 4.5) - 1, Mathf.Abs((int)(player.transform.position.y - 4.5)) + 1] = 5;
+                    stateMatrix[(int)(player.transform.position.x + 4.5) - 2, Mathf.Abs((int)(player.transform.position.y - 4.5)) + 2] = 5;
+                }
+                catch 
+                { }
+                break;
+            case (6):
+                try
+                {
+                    stateMatrix[(int)(player.transform.position.x + 4.5), Mathf.Abs((int)(player.transform.position.y - 4.5)) + 1] = 5;
+                    stateMatrix[(int)(player.transform.position.x + 4.5), Mathf.Abs((int)(player.transform.position.y - 4.5)) + 2] = 5;
+                }
+                catch 
+                { }
+                break;
+            case (7):
+                try
+                {
+                    stateMatrix[(int)(player.transform.position.x + 4.5) + 1, Mathf.Abs((int)(player.transform.position.y - 4.5)) + 1] = 5;
+                    stateMatrix[(int)(player.transform.position.x + 4.5) + 2, Mathf.Abs((int)(player.transform.position.y - 4.5)) + 2] = 5;
+                }
+                catch 
+                { }
+                break;
+            default:
+                break;
+        }
+
+        string sus;
+        for (int i = 0; i < 10; i++)
+        {
+            sus = "";
+            for (int j = 0; j < 10; j++)
+            {
+                sus += stateMatrix[i, j].ToString();
+            }
+            Debug.Log(sus);
+        }
+
+
+        return stateMatrix;
     }
 }
